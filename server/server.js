dd049bf8508c7ed562b1aff9a87673735b66d08f
MogesBekele <mogesbekele32@gmail.com>
2025-11-05 17:15:30 +0300

    create user inngest fuction

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
@@ -14,6 +16,8 @@ const PORT = 5000;
 app.get("/", (req, res) => {
   res.send("server is running ");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
