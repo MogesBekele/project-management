87cfd6bc35a4d438ec4ea121777dc7ac1c365367
MogesBekele <mogesbekele32@gmail.com>
2025-11-05 20:10:08 +0300

    inngest endpoint in server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 const PORT = 5000;
 
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("server is running ");
 });
 
+// inngest endpoint
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
