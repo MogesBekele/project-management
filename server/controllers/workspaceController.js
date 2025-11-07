0c8612a374ee23814457beeb2b04d4b2774ff121
MogesBekele <mogesbekele32@gmail.com>
2025-11-07 16:09:14 +0300

    create getuserworkspace in inngest

---
+++ server/controllers/workspaceController.js
@@ -0,0 +1,33 @@
+// get all workspaces for user
+
+import prisma from "../config/prisma";
+
+
+
+export const getUserWorkspaces = async (req, res )=>{
+  try {
+    const {userId}= await req.auth();
+    const workspaces = await prisma.workspace.findMany({
+      where:{
+        members:{some:{userId: userId}}
+      },
+      include:{
+        members:{include:{user:true}},
+        projects:{
+          include:{
+            tasks:{include:{assignee:true, comments:{
+              include:{user:true}
+            }}},
+            members:{include:{user:true}}
+          }
+        },
+        owner:true
+      }
+    })
+    res.status(200).json({workspaces})
+  } catch (error) {
+    console.error("Error fetching user workspaces:", error);
+    res.status(500).json({ error: "An error occurred while fetching workspaces." });
+    
+  }
+}
