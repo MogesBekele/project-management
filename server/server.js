985b50ea2714b7d1a58ef9aeeb7da2df49a264f3
MogesBekele <mogesbekele32@gmail.com>
2025-11-07 17:09:15 +0300

    middleware for workspace function

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from "./routes/workspaceRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
 
 const app = express();
 app.use(cors());
@@ -21,7 +22,7 @@ app.get("/", (req, res) => {
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //Routes
-app.use('/api/workspaces', workspaceRouter)
+app.use('/api/workspaces', protect, workspaceRouter)
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
